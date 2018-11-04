8cc7e230dda3a863612d6ac2db612f8a1be81039
Ricardo Guerrero <ricardo.guerrero@beeva.com>
2018-11-04 20:22:54 +0100

    Loading VGG-Imagenet weights [2].

--- Models/FCN8.py
+++ Models/FCN8.py
@@ -85,7 +85,8 @@ def FCN8( nClasses ,  input_height=416, input_width=608 , vgg_level=3):
 	x = Dense( 1000 , activation='softmax', name='predictions')(x)
 
 	vgg  = Model(  img_input , x  )
-	#vgg.load_weights(VGG_Weights_path)
+	vgg.load_weights(VGG_Weights_path)
+	print("VGG weights loaded.")
 
 	o = f5
 
